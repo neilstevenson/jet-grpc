b939a4bf1db65c0c73ebb51f852486244c5663be
Frantisek Hartman <frant.hartm@gmail.com>
2020-10-14 11:36:08 +0200

    Add retry for unary client

--- jet-server-grpc-client-unary/src/main/java/neil/demo/Application.java
+++ jet-server-grpc-client-unary/src/main/java/neil/demo/Application.java
@@ -2,25 +2,20 @@ package neil.demo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
-import com.hazelcast.function.BiConsumerEx;
-import com.hazelcast.function.FunctionEx;
-import com.hazelcast.function.SupplierEx;
 import com.hazelcast.jet.Jet;
 import com.hazelcast.jet.JetInstance;
 import com.hazelcast.jet.aggregate.AggregateOperations;
 import com.hazelcast.jet.config.JetConfig;
 import com.hazelcast.jet.grpc.GrpcService;
 import com.hazelcast.jet.grpc.GrpcServices;
-import com.hazelcast.jet.pipeline.BatchStage;
 import com.hazelcast.jet.pipeline.Pipeline;
 import com.hazelcast.jet.pipeline.ServiceFactory;
 import com.hazelcast.jet.pipeline.Sinks;
 import com.hazelcast.jet.pipeline.test.TestSources;
-
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import io.grpc.stub.StreamObserver;
 
 public class Application {
 
@@ -60,7 +55,18 @@ public class Application {
                     InputMessage inputMessage =
                             InputMessage.newBuilder().addAllInputValue(list).build();
  
-                    return service.call(inputMessage).thenApply(outputMessage -> {
+                    return retry(() -> service.call(inputMessage), 1).thenApply(outputMessage -> {
+                        if (outputMessage == null) {
+                            // Failed N times, record a failed batch
+                            System.out.println("Failed batch: " + list);
+
+                            // Need to return a list with null values to filter out the failed items
+                            ArrayList<String> failedBatch = new ArrayList<>();
+                            for (int i = 0; i < list.size(); i++) {
+                                failedBatch.add(null);
+                            }
+                            return failedBatch;
+                        }
                         System.out.println("---");
                         System.out.println();
                         System.out.println(inputMessage);
@@ -79,6 +85,25 @@ public class Application {
         .writeTo(Sinks.logger(o -> new String("Total: " + o)));
                 
         return pipeline;
+    }
+
+
+    private static <T> CompletableFuture<T> retry(Supplier<CompletableFuture<T>> supplier, int retry) {
+        if (retry < 0) {
+            // Exceeded number of retries, return null future, instead of throwing exception
+            CompletableFuture<T> f = new CompletableFuture<>();
+            f.complete(null);
+            return f;
+        }
+        System.out.println("Retry " + retry);
+        return supplier.get()
+                       .thenApply(CompletableFuture::completedFuture)
+                       .exceptionally(t -> {
+                           // Log t if needed
+
+                           return retry(supplier, retry - 1);
+                       })
+                       .thenCompose(Function.identity());
     }
     
     /*FIXME Generics. Java accepts this, but at least Eclipse doesn't.
